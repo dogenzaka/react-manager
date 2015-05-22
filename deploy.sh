5693b88db855d975bac355522258abee626071ed
Suguru Namura <snamura@gmail.com>
2015-05-22 23:41:29 +0900

    Fix app path

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # exit with nonzero exit code if anything fails
 
 # go to the out directory and create a *new* Git repo
-cd app
+cd build
 git init
 
 # inside this git repo we'll pretend to be a new user
